4a9e187005fbc672c815d8a7a5876bb45c03b8ce
jsanchezsinning-max <jsanchezsinning@gmail.com>
2026-04-14 10:27:53 -0500

    Add files via upload

---
+++ Salem/avance-pagina/script.js
@@ -0,0 +1,35 @@
+let slideIndex = 0;
+let slideTimer;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    clearTimeout(slideTimer); // Detener la transición automática al usar la navegación manual
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    clearTimeout(slideTimer); // Detener la transición automática al usar la navegación manual
+    showSlides(slideIndex = n - 1);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+
+    // Restablecer el temporizador para la transición automática
+    slideTimer = setTimeout(() => showSlides(slideIndex += 1), 7000);
+}
