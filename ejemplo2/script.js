a6ecea33d785dc7136bfa7a32c34601aac726108
jsanchezsinning-max <jsanchezsinning@gmail.com>
2026-04-23 10:27:57 -0500

    Update script.js

--- ejemplo2/script.js
+++ ejemplo2/script.js
@@ -1,4 +1,54 @@
 let fondo = document.querySelector('.fondo');
+let end = document.querySelector('.end');
+let animacion = false;
+
+function updatefondo(){
+    let movY = window.scrollY;
+    let maxScroll = 400;
+    let movimiento = Math.min(movY / maxScroll, 1);
+    let escala = 1 + movimiento * 5;
+    let opacidad = 1 - movimiento;
+
+    fondo.style.transform = `scale(${escala})`;
+    fondo.style.opacity = opacidad;
+}
+
+function updateEnd(){
+    let movY = window.scrollY;
+    let posicionEnd = end.offsetTop;
+    let alturaPantalla = window.innerHeight;
+
+    let distancia = movY + alturaPantalla - posicionEnd;
+
+    let maxScroll = alturaPantalla;
+    let movimiento = Math.min(distancia / maxScroll, 1);
+
+    let escala = 6 - movimiento * 5; 
+    let opacidad = movimiento;
+
+    if(distancia > 0){
+        end.style.transform = `scale(${escala})`;
+        end.style.opacity = opacidad;
+    }
+}
+
+
+window.addEventListener('scroll', () => {
+
+    if(!animacion){
+        animacion = true;
+        requestAnimationFrame(() => {
+            updatefondo(); 
+            updateEnd(); 
+            animacion = false;
+        });
+    }
+
+});
+
+/** funcion hecha con jander
+ * 
+let fondo = document.querySelector('.fondo');
 let animacion = false;
 
 function updatefondo(){
@@ -23,29 +73,4 @@ window.addEventListener('scroll', () => {
 
 
 })
-
-let end = document.querySelector('.end');
-let animacion2 = false;
-
-function updatefondo2(){
-    let movY = window.scrollY;
-    let maxScroll = 400;
-    let movimiento = Math.min(movY / maxScroll, 1);
-    let escala = 1 + movimiento * 5;
-    let opacidad = 1 - movimiento;
-
-    end.style.transform = `scale(${escala})`;
-    end.style.opacity = opacidad;
-    animacion2 = false;
-}
-
-window.addEventListener('scroll', () => {
-
-
-    if(!animacion2){
-        animacion2 = true;
-        requestAnimationFrame(updatefondo);
-    }
-
-
-})
+*/
