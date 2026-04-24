fd3f011abb4d29a0fad234b873c8398edb316427
jsanchezsinning-max <jsanchezsinning@gmail.com>
2026-04-24 14:18:54 -0500

    Update script.js

--- festival gourmet/script.js
+++ festival gourmet/script.js
@@ -23,3 +23,29 @@ window.addEventListener('scroll', () => {
 
 
 })
+
+window.addEventListener("load", () => {
+    const track = document.querySelector(".track");
+
+    if (!track) return;
+
+    // duplicar contenido
+    track.innerHTML += track.innerHTML;
+
+    // calcular ancho real
+    const trackWidth = track.scrollWidth / 2;
+
+    // crear animación dinámica
+    const style = document.createElement("style");
+    style.innerHTML = `
+    @keyframes scroll {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(-${trackWidth}px);
+        }
+    }
+    `;
+    document.head.appendChild(style);
+});
